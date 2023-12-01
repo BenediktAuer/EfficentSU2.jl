a0fdec0690e17ba16ab16ac5692e242f3c27dd48
BenediktAuer <benedikt_auer01@web.de>
2023-12-01 15:55:45 +0100

    added Tests & updated TODOs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using EfficentSU2
-using Test, StaticArrays
+using Test, StaticArrays,LinearAlgebra
 
 @testset "Instantiation and getFields" begin
     a=SU2(1+1f0*im,2f0*im)
@@ -7,7 +7,22 @@ using Test, StaticArrays
     @test a[1] == 1+1f0im
     @test a[2] == 2f0im
 end
-#TODO Add test for getMatrix()
-#TODO Add test for Base.*
-#TODO Add test for mul!
-#TODO Add test for similar
+
+@testset "getMatrix" begin
+    a = SU2(2+3f0im,1+4f0im)
+    @test getMatrix(a) == @MMatrix([[2.0+3.0im  1.0+4.0im];[-1+4im 2.0-3im]])
+end
+@testset "similar" begin
+    a = SU2(2+3f0im,1+4f0im)
+    @test typeof(a) == typeof(similar(a))
+end
+
+@testset "Multiplication" begin
+    a = SU2(1+1f0*im,2f0*im)
+    b = SU2(1+1f0*im,2f0*im)
+    c = similar(a)
+    @test getMatrix(a*b) == @MMatrix([[-4+2im  4.0im];[4im -4-2im]])
+    mul!(c,a,b)
+    @test getMatrix(c) == @MMatrix([[-4+2im  4.0im];[4im -4-2im]])
+end
+
