ce7c50c3f6d92e9aa03d5c5d2b646962ccdfd37b
BenediktAuer <benedikt_auer01@web.de>
2023-12-02 13:08:06 +0100

    updated Docstrings

--- src/EfficentSU2.jl
+++ src/EfficentSU2.jl
@@ -32,11 +32,7 @@ function getMatrix(a::SU2)
     #     return
     
     # end
- """
-    *(a::SU2,b::SU2)
-#TODO Add DFocstring
-TBW
-"""
+
 *(a::SU2,b::SU2)= SU2(a[1]*b[1]-a[2]*conj(b[2]), a[1]*b[2]+a[2]*conj(b[1]))
 tr(a::SU2) = a[1]+conj(a[1])
 
@@ -53,6 +49,25 @@ end
 function ones(::Type{T}, dims::Tuple{Vararg{I, N}} where I<:Integer) where {T<:SU2,N}
     return reshape([ones(T) for i in 1:prod(dims)],dims)
 end
+"""
+    renormalize!(a::SU2)
+Normalizes the Matrix `a`such that  ``\\abs{z_1}^1+\\abs{z_2} =1``
+
+```julia-repl
+julia> a = SU2(2+3f0im,1+4f0im)
+2-element SU2{ComplexF32} with indices SOneTo(2):
+ 2.0f0 + 3.0f0im
+ 1.0f0 + 4.0f0im
+ julia> renormalize!(a)
+ julia> a
+ 2-element SU2{ComplexF32} with indices SOneTo(2):
+ 0.36514837f0 + 0.5477225f0im
+ 0.18257418f0 + 0.73029673f0im
+ julia> sum(abs2,a)
+ 0.9999999f0
+
+```
+"""
 function renormalize!(a::SU2) 
      a./= norm(a)
     return
